fbff30c955a7adfa448bfcdc4817dd0c82a223ab
2bo <developer.2bo@gmail.com>
2020-09-08 00:23:11 +0900

    add helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    # コントローラとビューの両方から利用する機会があるので、helper_methodでメソッド名を宣言する
+    helper_method :logged_in?
+
+    private
+
+    def logged_in?
+        !!session[:user_id]
+    end
 end

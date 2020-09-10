14866b90238f31bc2dfc1585b8220ef6731598c7
2bo <developer.2bo@gmail.com>
2020-09-10 22:09:08 +0900

    add event list page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate
+
   def index
+    # 未開催のイベントを開催時間が近い順に取得
+    @events = Event.where("start_at > ?", Time.zone.now).order(:start_at)
   end
 end

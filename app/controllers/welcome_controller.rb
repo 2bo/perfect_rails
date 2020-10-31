85cf6e63fec28ae6f99d6460af4bf13a1604060f
2bo <developer.2bo@gmail.com>
2020-10-31 17:03:01 +0900

    add kaminari for pagination

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
 
   def index
     # 未開催のイベントを開催時間が近い順に取得
-    @events = Event.where("start_at > ?", Time.zone.now).order(:start_at)
+    @events = Event.page(params[:page]).per(10).
+    where("start_at > ?", Time.zone.now).order(:start_at)
   end
 end

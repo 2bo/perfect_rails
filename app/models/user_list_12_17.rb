b9d52a6c232cdaf32cc4f43bcc8f6286d64c970c
2bo <developer.2bo@gmail.com>
2020-11-03 19:12:19 +0900

    finish 12-3-3

---
+++ app/models/user_list_12_17.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+    validates :password, format: { with: /\A[\p{ascii}&&[^\x20]]{8,72}\z/, allow_nil: true }
+end
